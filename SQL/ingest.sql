cef58ab58cee77d0c0994c6e792ef86e738b0f66
Daniel Deniger <dannydeniger@gmail.com>
2023-07-01 09:39:29 -0700

    final week 1 commit

--- SQL/ingest.sql
+++ SQL/ingest.sql
@@ -1,24 +1,24 @@
 
+--Ingest data from files to duckdb
 
---bike_data, 
-CREATE TABLE bike_data AS SELECT * FROM '../data/data/bike/20*-citibike-tripdata.csv.gz';
+--bike_data, explicitly define start_station_id so it does not assume DOUBLE 
+CREATE TABLE bike_data AS SELECT * FROM read_csv_auto('../data/data/bike/20*-citibike-tripdata.csv.gz', union_by_name=True, types={'start_station_id': VARCHAR});
 --central_park_weather,
-CREATE TABLE central_park_weather AS SELECT * FROM '../data/data/central_park_weather.csv';
+CREATE TABLE central_park_weather AS SELECT * FROM read_csv_auto('../data/data/central_park_weather.csv', union_by_name=True);
 --daily_citi_bike_trip_counts_and_weather,
-CREATE TABLE daily_citi_bike_trip_counts_and_weather AS SELECT * FROM '../data/data/daily_citi_bike_trip_counts_and_weather.csv';
+CREATE TABLE daily_citi_bike_trip_counts_and_weather AS SELECT * FROM read_csv_auto('../data/data/daily_citi_bike_trip_counts_and_weather.csv', union_by_name=True);
 
---fhv_bases, 
-CREATE TABLE fhv_bases AS SELECT * FROM '../data/data/fhv_bases.csv';
+--fhv_bases, explicitly define header due to all VARCHAR cols
+CREATE TABLE fhv_bases AS SELECT * FROM read_csv_auto('../data/data/fhv_bases.csv', union_by_name=True, header=True);
 
 --fhv_tripdata, 
-CREATE TABLE fhv_tripdata AS SELECT * FROM read_parquet('../data/data/taxi/fhv_tripdata_20*.parquet');
+CREATE TABLE fhv_tripdata AS SELECT * FROM read_parquet('../data/data/taxi/fhv_tripdata_20*.parquet', union_by_name=True);
 
 --fhvhv_tripdata,
-CREATE TABLE fhvhv_tripdata AS SELECT * FROM read_parquet('../data/data/taxi/fhvhv_tripdata_20*.parquet');
+CREATE TABLE fhvhv_tripdata AS SELECT * FROM read_parquet('../data/data/taxi/fhvhv_tripdata_20*.parquet', union_by_name=True);
 
 --green_tripdata, 
-CREATE TABLE green_tripdata AS SELECT * FROM read_parquet('../data/data/taxi/green_tripdata_20*.parquet');
+CREATE TABLE green_tripdata AS SELECT * FROM read_parquet('../data/data/taxi/green_tripdata_20*.parquet', union_by_name=True);
 
 --yellow_tripdata
-CREATE TABLE yellow_tripdata AS SELECT * FROM read_parquet('../data/data/taxi/yellow_tripdata_20*.parquet');
-
+CREATE TABLE yellow_tripdata AS SELECT * FROM read_parquet('../data/data/taxi/yellow_tripdata_20*.parquet', union_by_name=True);
--- Scripts/run_week1.sh
+++ Scripts/run_week1.sh
@@ -1,7 +1,8 @@
-
-#aws s3 sync s3://cmu-95797-23m6 ../data/ --no-sign-request
+# ddeniger 7/1/2023
+# run_week1.sh : 
+# Orchestrate steps to download s3 data, ingest to duckdb, and validate schemas/counts
+aws s3 sync s3://cmu-95797-23m6 ../data/ --no-sign-request
 ../../duckdb main.db -s ".read ../SQL/ingest.sql"
-#../../duckdb main.db -s ".read ../SQL/dump_raw_schemas.sql" > ../Answers/raw_schemas.txt
-#../../duckdb main.db -s ".read ../SQL/dump_raw_counts.sql" > ../Answers/raw_counts.txt
+../../duckdb main.db -s ".read ../SQL/dump_raw_schemas.sql" > ../Answers/raw_schemas.txt
+../../duckdb main.db -s ".read ../SQL/dump_raw_counts.sql" > ../Answers/raw_counts.txt
 
-#python dump_raw_counts.py > ../Answers/raw_counts.txt
